ca0f4d1c632e3d97b6dbd126e47bf29efad5f284
Javier Urquizu <javierurquizu@gmail.com>
2023-06-26 13:54:17 -0700

    Finished install-gitrepos.sh

--- scripts/install-gitrepos.sh
+++ scripts/install-gitrepos.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+# Define valid flags
+declare -a valid_flags=("personal" "work" "containerd")
+
 # Function to display script usage
 display_usage() {
-    echo "Usage: $0 [--personal | --work | --containerd]"
+    echo "Usage: $0 [personal | work | containerd]"
 }
 
 # Declare global variables
-declare config_file="./config/gitconfig_template.json"
+declare config_file="./config/gitconfig.json"
 declare localpath
 declare -a parsed_required
 declare -a parsed_optional
@@ -42,7 +45,8 @@ parse_config() {
             parsed_required+=("https://${pat}@${repo}")
         fi
     done
-        for repo in "${opts[@]}"; do
+
+    for repo in "${opts[@]}"; do
         if [ -z "$pat" ]; then
             parsed_optional+=("https://${repo}")
         else
@@ -57,29 +61,24 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 
-# Parse input flags
-while [[ $# -gt 0 ]]; do
-    key="$1"
+# Check if more than one flag is provided
+if [ $# -gt 1 ]; then
+    echo "Error! Only one flag can be provided."
+    display_usage
+    exit 1
+fi
 
-    case $key in
-        --personal)
-        config_section="personal"
-        ;;
-        --work)
-        config_section="work"
-        ;;
-        --containerd)
-        config_section="containerd"
-        ;;
-        *)
-        # Invalid flag provided
-        display_usage
-        exit 1
-        ;;
-    esac
+# Get the flag from the command-line argument
+flag="$1"
 
-    shift
-done
+# Check if the flag is a valid flag
+if [[ " ${valid_flags[*]} " =~ " ${flag} " ]]; then
+    config_section="$flag"
+else
+    # Invalid flag provided
+    display_usage
+    exit 1
+fi
 
 # Check if the config file exists
 if [ ! -f "$config_file" ]; then
@@ -90,9 +89,44 @@ fi
 
 parse_config "$config_section"
 
-echo "[DEBUG] localpath (global): $localpath"
-echo "[DEBUG] pat (global): $pat"
-echo "[DEBUG] reqs (global): $reqs"
-echo "[DEBUG] opts (global): $opts"
-echo "[DEBUG] parsed_required (global): ${parsed_required[@]}"
-echo "[DEBUG] parsed_optional (global): ${parsed_optional[@]}"
+# Check if the localpath directory exists, create it if not
+if [ ! -d "$localpath" ]; then
+    if ! mkdir -p "$localpath"; then
+        echo "Error! Failed to create directory: $localpath"
+        exit 1
+    fi
+fi
+
+# Check if required repositories exist and clone them
+if [ ${#parsed_required[@]} -ne 0 ]; then
+    for repo in "${parsed_required[@]}"; do
+        repo_name=$(basename "$repo" .git)
+        if [ -d "$localpath$repo_name" ]; then
+            echo "Repository $repo_name already exists. Skipping cloning."
+        else
+            echo "git clone '$repo' '$localpath$repo_name'"
+            git clone "$repo" "$localpath$repo_name"
+        fi
+    done
+fi
+
+# Check if required repositories exist and clone them
+if [ ${#parsed_optional[@]} -ne 0 ]; then
+    for repo in "${parsed_optional[@]}"; do
+        repo_name=$(basename "$repo" .git)
+        if [ -d "$localpath$repo_name" ]; then
+            echo "Repository $repo_name already exists. Skipping cloning."
+        else
+            read -p "Do you want to install the repository $repo? [Y/N]: " choice
+            case $choice in
+                [Yy]*)
+                    echo "git clone '$repo' '$localpath$repo_name'"
+                    git clone "$repo" "$localpath$repo_name"
+                    ;;
+                *)
+                    echo "Skipping installation of optional repository."
+                    ;;
+            esac
+        fi
+    done
+fi
