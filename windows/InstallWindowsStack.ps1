e2e949dd8d539ac4e7151dfd70e8012baa915f07
Javier Urquizu <javier.urquizu@hp.com>
2023-07-17 12:44:20 -0700

    Added ps1 scripts for Installing Windows Stack (bug: optional pkgs aren't read) and Setting up WSL

---
+++ windows/InstallWindowsStack.ps1
@@ -0,0 +1,41 @@
+# Get the absolute path of the parent directory of the script
+$scriptPath = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
+$configPath = Join-Path -Path $scriptPath -ChildPath "config/windows_stack.json"
+
+# Read the contents of the config file
+$configContent = Get-Content -Path $configPath -Raw | ConvertFrom-Json
+
+# Access and use the configuration values
+$required = $configContent.REQUIRED
+$optional = $configContent.OPTIONAL
+
+# Function to check if a package is installed using winget
+function IsPackageInstalled($packageName) {
+    $packageList = winget list $packageName
+    return $packageList -match "$packageName"
+}
+
+# Iterate over required values and install the required items if not already installed
+foreach ($requiredItem in $required) {
+    if (!(IsPackageInstalled $requiredItem)) {
+        Write-Host "Installing required item: $requiredItem"
+        winget install -e --id $requiredItem --silent --accept-package-agreements --accept-source-agreements
+    }
+    else {
+        Write-Host "Required item $requiredItem is already installed"
+    }
+}
+
+# Iterate over optional values and prompt the user for confirmation
+foreach ($optionalItem in $optional) {
+    if (!(IsPackageInstalled $optionalItem)) {
+        $installOptional = Read-Host "Do you want to install the optional item: $optionalItem? (Y/N)"
+        if ($installOptional -eq "Y" -or $installOptional -eq "y") {
+            Write-Host "Installing optional item: $optionalItem"
+            winget install -e --id $optionalItem --silent --accept-package-agreements --accept-source-agreements
+        }
+    }
+    else {
+        Write-Host "Optional item $optionalItem is already installed"
+    }
+}
---
+++ windows/SetupWSL.ps1
@@ -0,0 +1,69 @@
+Param (
+    [switch]$InstallWSL2,
+    [switch]$SetDefaults,
+    [switch]$Unregister,
+    [switch]$UpdateWSL,
+    [string]$distro = "Ubuntu-20.04"
+)
+
+# Setup Transcript Logging
+$VerbosePreference = "Continue"
+$LogPath = Split-Path $MyInvocation.MyCommand.Path
+Get-ChildItem "$LogPath\*.log" | Where LastWriteTime -LT (Get-Date).AddDays(-15) | Remove-Item -Confirm:$false
+$LogPathName = Join-Path -Path $LogPath -ChildPath "$($MyInvocation.MyCommand.Name)-$(Get-Date -Format 'MM-dd-yyyy').log"
+Start-Transcript $LogPathName -Append
+
+$scriptName = $MyInvocation.MyCommand.Name
+Write-Verbose "Launching script: $scriptName"
+Write-Verbose "Starting location: $LogPath"
+
+function Install-HPDS-WSL2 {
+    Write-Verbose "$(Get-Date): Installing WSL2..."
+    wsl --install -d $distro
+    Write-Verbose "$(Get-Date): Installed WSL2!"
+}
+
+function Resync-HPDS-Windows-Time {
+    Write-Verbose "$(Get-Date): Re-Synchronizing Windows Time..."
+    net stop w32time
+    w32tm /unregister
+    w32tm /register
+    net start w32time
+    w32tm /resync /nowait
+    Write-Verbose "$(Get-Date): Re-Sync done!"
+}
+
+function Set-HPDS-WSL2-Defaults {
+    Write-Verbose "$(Get-Date): Setting Defaults for WSL2..."
+    wsl --set-default-version 2
+    wsl --setdefault $distro
+    wsl --set-version $distro 2
+    Write-Verbose "$(Get-Date): Set Defaults for WSL2!"
+}
+
+function Unregister-HPDS-Distro {
+    Write-Verbose "$(Get-Date): Unregistering distro: $distro"
+    wsl --unregister $distro
+    Write-Verbose "$(Get-Date): Unregistered distro!"
+}
+
+function Update-HPDS-WSL {
+    Write-Verbose "$(Get-Date): Updating WSL!"
+    wsl --update
+    Write-Verbose "$(Get-Date): Updated WSL!"
+}
+
+# Main
+if ($InstallWSL2) {
+    Resync-HPDS-Windows-Time
+    Install-HPDS-WSL2
+}
+if ($SetDefaults) {
+    Set-HPDS-WSL2-Defaults
+}
+if ($Unregister) {
+    Unregister-HPDS-Distro
+}
+if ($UpdateWSL) {
+    Update-HPDS-WSL
+}
