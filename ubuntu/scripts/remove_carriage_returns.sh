d48a0d7585a09ccbf1fc89fb838d86058974bc69
Javier Urquizu <javier.urquizu@hp.com>
2023-08-09 15:50:48 -0700

    Added ubuntu/remove_carrage_returns.sh

---
+++ ubuntu/scripts/remove_carriage_returns.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [ $# -ne 2 ]; then
+    echo "This script removes the Windows carriage return character (^M) from input_file & saves it into output_file"
+    echo "Usage: $0 input_file output_file"
+    exit 1
+fi
+
+input_file="$1"
+output_file="$2"
+
+# Use 'tr' to remove the Windows carriage return characters (^M)
+tr -d '\r' < "$input_file" > "$output_file"
+
+echo "Carriage return characters removed from $input_file and saved to $output_file"
