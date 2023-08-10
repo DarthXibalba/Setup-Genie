f3579b963d39e7744f1b3c45dbfb35431a881329
Javier Urquizu <javier.urquizu@hp.com>
2023-08-09 18:59:37 -0700

    reorganized ubuntu/setup-files/profile -> ubuntu/profile

--- ubuntu/scripts/copy-bash-profile.sh
+++ ubuntu/scripts/copy-bash-profile.sh
@@ -4,10 +4,10 @@
 script_dir="$(dirname "$(realpath "$0")")"
 
 # Copy profile
-cp "$script_dir/../setup-files/profile/.bashrc" "$HOME/.bashrc"
-cp "$script_dir/../setup-files/profile/.bash_aliases" "$HOME/.bash_aliases"
-cp "$script_dir/../setup-files/profile/.conda_aliases" "$HOME/.conda_aliases"
-cp "$script_dir/../setup-files/profile/.golang_phoenix_aliases" "$HOME/.golang_phoenix_aliases"
-cp "$script_dir/../setup-files/profile/.nerdctl_phoenix_aliases" "$HOME/.nerdctl_phoenix_aliases"
+cp "$script_dir/../profile/.bashrc" "$HOME/.bashrc"
+cp "$script_dir/../profile/.bash_aliases" "$HOME/.bash_aliases"
+cp "$script_dir/../profile/.conda_aliases" "$HOME/.conda_aliases"
+cp "$script_dir/../profile/.golang_phoenix_aliases" "$HOME/.golang_phoenix_aliases"
+cp "$script_dir/../profile/.nerdctl_phoenix_aliases" "$HOME/.nerdctl_phoenix_aliases"
 
 echo "Copied bash profile. Please run $ source ~/.bashrc to reload profile"
