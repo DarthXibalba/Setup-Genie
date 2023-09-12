60ba65e40f71b32f904175070f8f83bdfda1aa59
Javier Urquizu <javierurquizu@gmail.com>
2023-09-11 17:16:37 -0700

    Updated install-google-chrome.sh to use native apt-get install

--- ubuntu/scripts/install-google-chrome.sh
+++ ubuntu/scripts/install-google-chrome.sh
@@ -2,12 +2,6 @@
 # Get the absolute path of the script directory
 script_dir="$(dirname "$(realpath "$0")")"
 apt_get_install="$script_dir/helper_scripts/apt-get-install.sh"
-wget_download="$script_dir/helper_scripts/wget-download.sh"
 
-# Download Google Chrome deb
-gChromePath="$script_dir/../setup-files/google-chrome-stable_current_amd64.deb"
-gChromeURL="https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
-$wget_download $gChromeURL $gChromePath
-
-# Install the Google Chrome deb
-$apt_get_install $gChromePath
+# Install the Google Chrome
+$apt_get_install google-chrome-stable
