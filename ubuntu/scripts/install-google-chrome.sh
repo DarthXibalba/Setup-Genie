d3f0be36e6a0eac55c4a24e592ee6a9c44f9f9a2
Javier Urquizu <javierurquizu@gmail.com>
2024-06-26 12:40:39 -0700

    changed install-google-chrome.sh to install via dpkg that is wgot

--- ubuntu/scripts/install-google-chrome.sh
+++ ubuntu/scripts/install-google-chrome.sh
@@ -5,3 +5,8 @@ apt_get_install="$script_dir/helper_scripts/apt-get-install.sh"
 
 # Install the Google Chrome
 $apt_get_install google-chrome-stable
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+# If dependency errors occur run the following:
+# sudo apt --fix-broken install
