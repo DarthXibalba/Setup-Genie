e4eb75f784948bd511eb5eeb4d2c67ffed2f92f1
Javier Urquizu <javierurquizu@gmail.com>
2024-07-10 15:16:19 -0700

    revised setup.sh to now be run non-locally

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -2,7 +2,16 @@
 
 # Function to display script usage
 display_usage() {
-    echo "Usage: $0 <config_file> [<section>...]"
+    echo "Usage: $0 <section>..."
+}
+
+# Function to read config values from JSON file
+read_config() {
+    local json_file=$1
+    local section=$2
+    local key=$3
+
+    jq -r ".$section.$key[]" "$json_file"
 }
 
 # Check if jq is installed
@@ -12,79 +21,61 @@ if ! dpkg -s jq >/dev/null 2>&1; then
     sudo apt-get install jq -y
 fi
 
-# Read config values from JSON file
-read_config() {
-    local json_file=$1
-    local section=$2
-    local key=$3
+# Get absolute path of config_file
+script_dir=$(dirname "$(realpath "$0")")
+config_file="$script_dir/config/env_setup.json"
 
-    jq -r ".$section.$key[]" "$json_file"
-    ##jq -r ".$section.$key[]" "$json_file" | jq -r '.[]'
-}
-
-# Check if a config file is provided
-if [ -z "$1" ]; then
-    echo "Error! Config file not specified!"
-    display_usage
+# Check if config file exists
+if [ ! -f "$config_file" ]; then
+    echo "Error! Config file '$config_file' not found!"
     exit 1
 fi
 
-config_file=$1
-
-# Check if sections are provided
-if [ $# -lt 2 ]; then
+# Check if sections are provided as arguments
+if [ $# -lt 1 ]; then
     echo "Error! No sections specified!"
     display_usage
     exit 1
 fi
 
-# Change directory to where this script resides
-cd "$(dirname "$0")"
-
 # Read config values from the JSON file for each section
-for ((i = 2; i <= $#; i++)); do
-    section="${!i}"
+for section in "$@"; do
     required_scripts=($(read_config "$config_file" "$section" "REQUIRED"))
     optional_scripts=($(read_config "$config_file" "$section" "OPTIONAL"))
 
     # Check if required scripts are defined
     if [ ${#required_scripts[@]} -eq 0 ]; then
-        echo "Error! No required scripts defined for section: $section"
-        display_usage
-        exit 1
+        echo "Warning! No required scripts defined for section: $section"
+        continue
+    else
+        echo "Running scripts for section: $section"
+        for script in "${required_scripts[@]}"; do
+            # Convert '@' to whitespace in the script content
+            converted_script=${script//@/ }
+            echo "Running script: $converted_script"
+            bash -c "$converted_script"
+            echo "Finished running script: $converted_script"
+            echo ""
+        done
+
+        # Run the optional scripts
+        for script in "${optional_scripts[@]}"; do
+            # Convert '@' to whitespace in the script content
+            converted_script=${script//@/ }
+            # Prompt to execute script
+            read -rp "Do you want to run '$converted_script'? (Y/N): " choice
+            if [[ $choice =~ ^[Yy]$ ]]; then
+                echo "Running optional script: $converted_script"
+                bash -c "$converted_script"
+                echo "Finished running optional script: $converted_script"
+                echo ""
+            else
+                echo "Skipping optional script: $converted_script"
+                echo ""
+            fi
+        done
+
+        echo "Finished running scripts for section: $section"
+        echo ""
     fi
-
-    echo "Running scripts for section: $section"
-
-# Run the required scripts
-for script in "${required_scripts[@]}"; do
-    # Convert '@' to whitespace in the script content
-    converted_script=${script//@/ }
-    # Run the script
-    echo "Running script: $converted_script"
-    bash -c "$converted_script"
-    echo "Finished running script: $converted_script"
-    echo ""
-done
-
-
-    # Run the optional scripts
-    for script in "${optional_scripts[@]}"; do
-        # Convert '@' to whitespace in the script content
-        converted_script=${script//@/ }
-        # Prompt to execute script
-        read -rp "Do you want to run '$converted_script'? (Y/N): " choice
-        if [[ $choice =~ ^[Yy]$ ]]; then
-            echo "Running optional script: $converted_script"
-            bash "$script"
-            echo "Finished running optional script: $converted_script"
-            echo ""
-        else
-            echo "Skipping optional script: $converted_script"
-            echo ""
-        fi
-    done
-
-    echo "Finished running scripts for section: $section"
-    echo ""
 done
