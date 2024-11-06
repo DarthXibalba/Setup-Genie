7cb35b41318fbd16a5da76d11779a025dfd3aec1
Javier Urquizu <javier.urquizu@hp.com>
2024-11-06 15:17:23 -0800

    setup-bash-profile.sh now automatically copies all files in ubuntu/profile/

--- ubuntu/scripts/setup-bash-profile.sh
+++ ubuntu/scripts/setup-bash-profile.sh
@@ -5,34 +5,42 @@ script_dir="$(dirname "$(realpath "$0")")"
 profile_dir="$script_dir/../profile"
 rm_carriage_rtn="$script_dir/../helper-scripts/remove-carriage-returns.sh"
 
-# List of files to process
-files=(
-  ".bashrc"
-  ".bash_aliases"
-  ".conda_aliases"
-  ".golang_phoenix_aliases"
-  ".nerdctl_phoenix_aliases"
-)
+idx=0
+filesSrc=()
+filesTmp=()
+filesDst=()
+tmp_dir="$profile_dir/tmp"
+dst_dir="$HOME"
+
+# Check if the profile directory exists
+if [ ! -d "$profile_dir" ]; then
+    echo "Error: Profile directory does not exist at $profile_dir"
+    exit 1
+fi
+
+# Loop through all files in profile_dir and add to filesSrc and filesDst
+while IFS= read -r -d '' file; do
+    filesSrc[idx]="$file"
+    filesTmp[idx]="${file/$profile_dir/$tmp_dir}"
+    filesDst[idx]="${file/$profile_dir/$dst_dir}"
+    ((idx++))
+done < <(find "$profile_dir" -type f -print0)
 
 # Remove carriage return artifacts due to cross-platform (Windows) development
-temp_dir="$profile_dir/tmp"
-mkdir "$temp_dir"
-
-for file in "${files[@]}"; do
-  source_file="$profile_dir/$file"
-  target_file="$temp_dir/$file"
-  "$rm_carriage_rtn" "$source_file" "$target_file"
+mkdir "$tmp_dir"
+for ((i = 0; i < ${#filesSrc[@]}; i++)); do
+    echo "$rm_carriage_rtn ${filesSrc[i]} ${filesTmp[i]}"
+    "$rm_carriage_rtn" "${filesSrc[i]}" "${filesTmp[i]}"
 done
 
 # Move profile files to $HOME
-for file in "${files[@]}"; do
-  source_file="$temp_dir/$file"
-  target_file="$HOME/$file"
-  mv "$source_file" "$target_file"
+for ((i = 0; i < ${#filesTmp[@]}; i++)); do
+    echo "mv ${filesTmp[i]} ${filesDst[i]}"
+    mv "${filesTmp[i]}" "${filesDst[i]}"
 done
 
 # Cleanup and exit
-rm -rf "$temp_dir"
-echo "Deleted $temp_dir"
+rm -rf "$tmp_dir"
+echo "Deleted $tmp_dir"
 echo "Copied bash profile."
 echo "Please run $ source ~/.bashrc to reload profile"
