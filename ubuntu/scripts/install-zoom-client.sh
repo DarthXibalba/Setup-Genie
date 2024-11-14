0fe2c19f148d769c4a24a758d3c54f369c90afb3
Javier Urquizu <javier.urquizu@hp.com>
2024-11-14 14:50:01 -0800

    added install-zoom-client.sh

--- ubuntu/scripts/install-zoom-client.sh
+++ ubuntu/scripts/install-zoom-client.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 # Get the absolute path of the script directory
 script_dir="$(dirname "$(realpath "$0")")"
-apt_get_install="$script_dir/../helper-scripts/apt-get-install.sh"
 wget_download="$script_dir/../helper-scripts/wget-download.sh"
 
 # Download the RustDesk Software
-version="1.1.8"
-rustPath="$script_dir/../setup-files/rustdesk-$version.deb"
-rustURL="https://github.com/rustdesk/rustdesk/releases/download/$version/rustdesk-$version.deb"
+version="6.2.6.2503"
+zoomPath="/tmp/zoom_amd64.deb"
+zoomURL="https://zoom.us/client/$version/zoom_amd64.deb"
 $wget_download $rustURL $rustPath
 
 # Install the RustDesk Software
