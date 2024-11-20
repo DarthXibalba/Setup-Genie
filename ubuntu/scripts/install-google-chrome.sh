8847189128ad264f9beb6024d7877ae553afd42a
Javier Urquizu <javier.urquizu@hp.com>
2024-11-20 11:04:57 -0800

    fixed install-google-chrome.sh

--- ubuntu/scripts/install-google-chrome.sh
+++ ubuntu/scripts/install-google-chrome.sh
@@ -2,11 +2,15 @@
 # Get the absolute path of the script directory
 script_dir="$(dirname "$(realpath "$0")")"
 apt_get_install="$script_dir/../helper-scripts/apt-get-install.sh"
+wget_download="$script_dir/../helper-scripts/wget-download.sh"
 
 # Install the Google Chrome
-$apt_get_install google-chrome-stable
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
+##$apt_get_install google-chrome-stable
+
+gChromePath="/tmp/google-chrome-stable_current_amd64.deb"
+gChromeURL="https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
+$wget_download "$gChromeURL" "$gChromePath"
+sudo dpkg -i "$gChromePath"
 
 # If dependency errors occur run the following:
 # sudo apt --fix-broken install
