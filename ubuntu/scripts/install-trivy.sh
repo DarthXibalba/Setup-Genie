6b8620a99b4e677575eac053bacbdd3ab389a7a9
Javier Urquizu <javier.urquizu@hp.com>
2024-11-22 06:40:41 -0800

    Added install-trivy.sh

---
+++ ubuntu/scripts/install-trivy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Get the absolute path of the script directory
+script_dir="$(dirname "$(realpath "$0")")"
+apt_get_install="$script_dir/../helper-scripts/apt-get-install.sh"
+
+sudo apt-get install wget apt-transport-https gnupg
+wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | gpg --dearmor | sudo tee /usr/share/keyrings/trivy.gpg > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/trivy.gpg] https://aquasecurity.github.io/trivy-repo/deb generic main" | sudo tee -a /etc/apt/sources.list.d/trivy.list
+$apt_get_install trivy
