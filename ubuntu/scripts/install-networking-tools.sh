5f95dea97d4ffdee25e0c812800b5fea162ab9a2
Javier Urquizu <javier.urquizu@hp.com>
2025-02-25 18:31:49 -0800

    Added net-tools to common installation

--- ubuntu/scripts/install-networking-tools.sh
+++ ubuntu/scripts/install-networking-tools.sh
@@ -4,4 +4,4 @@ script_dir="$(dirname "$(realpath "$0")")"
 apt_get_install="$script_dir/../helper-scripts/apt-get-install.sh"
 
 # Install dependencies
-$apt_get_install curl git wget
+$apt_get_install curl git wget net-tools
