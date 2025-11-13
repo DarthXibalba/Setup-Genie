b6ef8006ce06639053d147c0c6e239ebc157ba9b
Javier Urquizu <javierurquizu@gmail.com>
2025-11-13 00:08:40 -0800

    Added install-veracrypt.sh

--- ubuntu/scripts/install-veracrypt.sh
+++ ubuntu/scripts/install-veracrypt.sh
@@ -5,11 +5,12 @@ script_dir="$(dirname "$(realpath "$0")")"
 apt_get_install="$script_dir/../helper-scripts/apt-get-install.sh"
 wget_download="$script_dir/../helper-scripts/wget-download.sh"
 
-version="6.5.11.4015"
-zoomPath="/tmp/zoom_amd64.deb"
-zoomURL="https://zoom.us/client/$version/zoom_amd64.deb"
+version="1.26.24"
+osarch="Ubuntu-22.04-amd64"
+downloadPath="/tmp/veracrypt-$version-$osarch.deb"
+url="https://launchpad.net/veracrypt/trunk/$version/+download/veracrypt-$version-$osarch.deb"
 
 # Download
-$wget_download $zoomURL $zoomPath
+$wget_download $url $downloadPath
 # Install
-$apt_get_install $zoomPath
+$apt_get_install $downloadPath
