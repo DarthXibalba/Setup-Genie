cd5a7dd605a68d5151700809aeaafb67790f1a5b
Javier Urquizu <javierurquizu@gmail.com>
2026-01-27 18:27:13 -0800

    Minor tweaks

--- ubuntu/scripts/install-kvm-qemu-virtmanager.sh
+++ ubuntu/scripts/install-kvm-qemu-virtmanager.sh
@@ -3,3 +3,7 @@
 sudo apt install -y qemu-kvm libvirt-daemon-system virt-manager
 sudo usermod -aG libvirt $USER
 reboot
+
+## After install be sure to run:
+# sudo systemctl start libvirtd
+# sudo systemctl enable libvirtd
